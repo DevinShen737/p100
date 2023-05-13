255d796e728375d2c1ddf1891dcb7725922adcf5
DevinShen737 <120994029+DevinShen737@users.noreply.github.com>
2023-05-13 14:08:21 -0700

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,9 +1,15 @@
 // Your web app's Firebase configuration
 //ADD YOUR FIREBASE LINKS HERE
-
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+    apiKey: "AIzaSyBCwqXh5C6D2pBNnDh48iHuHgOFDg5ylMY",
+    authDomain: "kwitter-b601d.firebaseapp.com",
+    databaseURL: "https://kwitter-b601d-default-rtdb.firebaseio.com",
+    projectId: "kwitter-b601d",
+    storageBucket: "kwitter-b601d.appspot.com",
+    messagingSenderId: "427931998389",
+    appId: "1:427931998389:web:83ab0f12e2c574d72406fc"
+  };
+  
 
 
 function addUser()
