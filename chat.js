7c02fde5e01b0fb8714d3f1fab55bf6357c79790
DevinShen737 <120994029+DevinShen737@users.noreply.github.com>
2023-05-20 09:18:41 -0700

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,14 +1,19 @@
 // Your web app's Firebase configuration
 //ADD YOUR FIREBASE LINKS HERE
 const firebaseConfig = {
-    apiKey: "AIzaSyBCwqXh5C6D2pBNnDh48iHuHgOFDg5ylMY",
-    authDomain: "kwitter-b601d.firebaseapp.com",
-    databaseURL: "https://kwitter-b601d-default-rtdb.firebaseio.com",
-    projectId: "kwitter-b601d",
-    storageBucket: "kwitter-b601d.appspot.com",
-    messagingSenderId: "427931998389",
-    appId: "1:427931998389:web:83ab0f12e2c574d72406fc"
-  };
+  apiKey: "AIzaSyBJ7O0XB5tKBKmr72THqqfr1VjC-O-refE",
+  authDomain: "cute-bird-69e12.firebaseapp.com",
+  databaseURL: "https://cute-bird-69e12-default-rtdb.firebaseio.com",
+  projectId: "cute-bird-69e12",
+  storageBucket: "cute-bird-69e12.appspot.com",
+  messagingSenderId: "273153165210",
+  appId: "1:273153165210:web:dd8b3054e513a269c51c23",
+  measurementId: "G-VNE5RDXCD7"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
   
 
 
